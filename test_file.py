bf369ead3a2d14f1753c14328a9b5f9f882fb7e6
Ned Webster <edwardpwebster@gmail.com>
2023-07-03 08:58:39 +0100

    add outputs to github output

--- test_file.py
+++ test_file.py
@@ -1,3 +1,5 @@
 import os
 
-print(os.environ["TEST_ENV_VAR"])
+if os.environ.get("TEST_ENV_VAR"):
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        print(f'env_var={os.environ.get("TEST_ENV_VAR")}', file=fh)
