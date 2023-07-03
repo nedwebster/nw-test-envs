797f2528b3e5852a8135f5e0d69483c59b50d929
Ned Webster <edwardpwebster@gmail.com>
2023-07-03 09:11:41 +0100

    add blank check

--- my_script.sh
+++ my_script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if [ "$TEST_ENV_VAR" -eq "I am in development" ]; then
+if [ -z "$TEST_ENV_VAR" ]; then
+    echo "env is blank!"
+elif [ "$TEST_ENV_VAR" == "I am in development" ]; then
     echo "The environment variable is set correctly!"
 else
     echo "The environment is not correct."
