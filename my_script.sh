bb85e4494742addf745425d0c98c25d2a0833171
Ned Webster <edwardpwebster@gmail.com>
2023-07-03 09:23:30 +0100

    new test

--- my_script.sh
+++ my_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ -z "$TEST_ENV_VAR" ]; then
+if [ -z "$MY_VAR" ]; then
     echo "env is blank!"
-elif [ "$TEST_ENV_VAR" == "I am in development" ]; then
+elif [ "$MY_VAR" == "I am in development" ]; then
     echo "The environment variable is set correctly!"
 else
     echo "The environment is not correct."
